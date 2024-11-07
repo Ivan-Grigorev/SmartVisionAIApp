295e43bcf46e53abea59382d50f6e046ed8535b1
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-11-07 13:45:50 +0100

    Remove simple_formatter

--- src/services/logging_config.py
+++ src/services/logging_config.py
@@ -19,32 +19,26 @@ detailed_formatter = ColoredFormatter(
     log_colors=log_colors,
 )
 
-simple_formatter = ColoredFormatter('%(log_color)s%(message)s', log_colors=log_colors)
-
 # File formatter without color, for detailed logging to a file
 file_formatter = logging.Formatter(
     '%(asctime)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S'
 )
 
 
-def setup_logger(logger_name, use_simple_formatter=False):
+def setup_logger(logger_name):
     """
     Set up a logger instance with the specified name using the configured ColoredFormatter and
     writes all logs to a file.
 
     Attributes:
         logger_name (str): The name of the logger instance.
-        use_simple_formatter (bool): Whether to use the simple formatter for console output.
 
     Returns:
         logging.Logger: The configured logger instance.
     """
     # Stream handler for console with colored output
     stream_handler = logging.StreamHandler()
-    if use_simple_formatter:
-        stream_handler.setFormatter(simple_formatter)
-    else:
-        stream_handler.setFormatter(detailed_formatter)
+    stream_handler.setFormatter(detailed_formatter)
 
     # File handler for logging detailed information to app.log
     file_handler = logging.FileHandler('app.log', encoding='utf-8')
