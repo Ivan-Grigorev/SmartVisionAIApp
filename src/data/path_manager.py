18c781b3d7c1ddfe1d8f6f6c3033344d772eba85
Ivan Grigorev <iv.st.grigorev@gmail.com>
2024-11-21 12:14:02 +0100

    Create app storage using default OS storage path

--- src/data/path_manager.py
+++ src/data/path_manager.py
@@ -1,22 +1,52 @@
 """Module to check and create a path to data files."""
 
 import os
+import platform
 import sys
+
+
+def app_storage(op_system, filename):
+    """
+    Create and return the full path to a file in the application specific storage directory.
+
+    Attributes:
+        op_system (str): The operating system name.
+        filename (str): The name of the file to create or access in the app storage.
+
+    Returns:
+        str: The full path to the file in the app's storage directory.
+    """
+    if op_system == 'Darwin':  # macOS
+        app_support_dir = os.path.expanduser('~/Library/Application Support/SmartVisionAI/')
+    elif op_system == 'Windows':
+        app_support_dir = os.path.expanduser('~/AppData/Local/SmartVisionAI/')
+    else:
+        # Other operating systems
+        app_support_dir = os.path.expanduser('~/.SmartVisionAI')
+
+    os.makedirs(app_support_dir, exist_ok=True)
+
+    return os.path.join(app_support_dir, filename)
 
 
 def get_data_file_path(filename):
     """
-    Return the full path to a specified data file, adjusting for PyInstaller bundles.
+    Return the full path to a specified data file, handling PyInstaller bundles and local source runs.
 
     Attributes:
-        filename (str): The name of the file to locate within the `data` directory.
+        filename (str): The name of the file to locate or create.
 
     Returns:
-        str: The full file path to `filename` within the `data` directory.
+        str: The full file path to the data file.
     """
+
+    # Check if running in PyInstaller bundle
     if hasattr(sys, '_MEIPASS'):
-        # When running in a PyInstaller bundle
-        return os.path.join(sys._MEIPASS, 'data', filename)
+        # Get the operating system name
+        op_system = platform.system()
+        file_path = app_storage(op_system, filename)
     else:
         # When running directly from the source
-        return os.path.join('src', 'data', filename)
+        file_path = os.path.join('src', 'data', filename)
+
+    return file_path
